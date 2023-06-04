29a08ce5486aa01fd6873c948d4c265e12f217be
Satya Padala <satya.eee14@gmail.com>
2023-06-04 06:37:01 +0000

    ssh tunnel via http

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gliderlabs/ssh"
+)
+
+type Tunnel struct {
+	w      io.Writer
+	doneCh chan struct{}
+}
+
+var tunnels = map[int]chan Tunnel{}
+
+func main() {
+
+	go func() {
+		http.HandleFunc("/", handleRequest)
+		log.Fatal(http.ListenAndServe(":3000", nil))
+	}()
+
+	ssh.Handle(func(s ssh.Session) {
+		id := rand.Intn(math.MaxInt)
+		tunnels[id] = make(chan Tunnel)
+		fmt.Println("tunnel ID: ", id)
+		tunnel := <-tunnels[id]
+
+		fmt.Println("tunnel is ready")
+
+		_, err := io.Copy(tunnel.w, s)
+		if err != nil {
+			log.Fatal("error while copying tunnel writer to ssh reader", err)
+		}
+
+		close(tunnel.doneCh)
+		s.Write([]byte("we are done!!!"))
+	})
+
+	log.Fatal(ssh.ListenAndServe(":2020", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, _ := strconv.Atoi(idStr)
+
+	tunnel, ok := tunnels[id]
+
+	if !ok {
+		w.Write([]byte("tunnel not found"))
+		return
+	}
+
+	doneCh := make(chan struct{})
+	tunnel <- Tunnel{
+		w:      w,
+		doneCh: doneCh,
+	}
+
+	<-doneCh
+}
